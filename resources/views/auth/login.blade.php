96d0754953bc63f4312cdfd6d5f58af49c1a3e90
LaXnZ <sumudithalanz@gmail.com>
2023-09-07 19:07:22 +0530

    minor modifications in signin and signup

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,8 +35,8 @@
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    {{ __("Don't have a account yet ?") }}
                 </a>
             @endif
 
